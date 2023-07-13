0faa437610db46cadf808a418e3cff37e8d8e015
etai tavor <mntetai@gmail.com>
2023-07-13 21:15:48 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,24 @@
+resource "aws_instance" "web" {
+  ami = "ami-095944276e3716f29"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+  security_groups = [aws_security_group.web.id]
+}
 
+resource "aws_vpc" "public" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.public.id
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_security_group" "web" {
+  name = "web"
+  ingress {
+    protocol = "tcp"
+    port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
