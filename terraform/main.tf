cc39db014e4d256ee55b18ff5480adf142a430d8
etai tavor <mntetai@gmail.com>
2023-07-13 23:33:46 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,32 @@
-resource "aws_instance" "web" {
-  ami = "ami-095944276e3716f29"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.id
-  security_groups = [aws_security_group.web.id]
-}
+#resource "aws_instance" "web" {
+#  ami = "ami-095944276e3716f29"
+#  instance_type = "t2.micro"
+#  subnet_id = aws_subnet.public.id
+#  security_groups = [aws_security_group.web.id]
+#}
 
-resource "aws_vpc" "public" {
-  cidr_block = "10.0.0.0/16"
-}
+#resource "aws_vpc" "public" {
+#  cidr_block = "10.0.0.0/16"
+#}
 
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.public.id
-  cidr_block = "10.0.0.0/24"
-}
+#resource "aws_subnet" "public" {
+#  vpc_id = aws_vpc.public.id
+#  cidr_block = "10.0.0.0/24"
+#}
 
-resource "aws_security_group" "web" {
-  name = "web"
-  vpc_id = aws_vpc.public.id
-  ingress {
-    protocol = "tcp"
-    from_port = 80
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#resource "aws_security_group" "web" {
+#  name = "web"
+#  vpc_id = aws_vpc.public.id
+#  ingress {
+#    protocol = "tcp"
+#   from_port = 80
+#    to_port = 80
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
